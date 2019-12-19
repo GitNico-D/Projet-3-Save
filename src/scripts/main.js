f9fd887f41eabbf51c8805384720d33c7907df67
GitNico-D <niicolas.dubief@gmail.com>
2019-12-19 18:22:16 +0100

    Scripts

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    let slideOne = new Slide ("slide_one", "blue", 1);
+    // let slideTwo = new Slide ("slide_two", "red", 2);
+    // let slideThree = new Slide ("slide_three", "yellow", 3);
+    // let slideFour = new Slide ("slide_four", "green", 4);
+    // let slideFifth = new Slide ("slide_fifth", "green", 4);
+}); 
---
+++ src/scripts/slider.js
@@ -0,0 +1,89 @@
+class Slide {
+    constructor (slideId, color, position) {
+        this.slideId = slideId;
+        this.color = color;
+        this.position = position;
+        console.log(this);
+        // this.createSlide();        
+        $("#next_slide").click(this.slideRightChange.bind(this));
+        this.start = false;
+        this.sliderAutoChange();
+        $("#play_pause_slider").click(this.sliderAutoChange.bind(this));
+        $("#previous_slide").click(this.slideLeftChange.bind(this));
+        $(document).keydown(this.sliderKeyboardChange.bind(this));
+    }
+    
+    // createSlide() {
+    //     let slide = `<div id="${this.name}" class="slide_img ${this.color}">
+    //                     <span class="text_content_slide">SLIDE ${this.position}</span>
+    //                     <div class="background_arrow_right"></div>
+    //                     <div class="background_arrow_left"></div>
+    //                 </div>`
+    //     $("#slider").append(slide);
+    //     if (this.position === 1) {
+    //         $(`#${this.name}`).addClass("active_slide");
+    //     }
+    // }
+
+    sliderAutoChange() {
+        if (!this.start) {
+            $("#play_pause_slider").removeClass("fa-play-circle");         
+            this.interval = setInterval(this.slideRightChange.bind(this), 5000)
+            $("#play_pause_slider").addClass("fa-pause-circle");
+            console.log("Start Slider !");
+        } else {
+            $("#play_pause_slider").removeClass("fa-pause-circle");
+            clearInterval(this.interval); 
+            $("#play_pause_slider").addClass("fa-play-circle");
+            console.log("Pause Slider !")             
+        }
+        this.start=!this.start;
+    }
+
+    slideRightChange() {
+        let $allSlides = $(".slide_img") // A mettre dans conctrustor ??
+        let $currentSlide = $(".active_slide");
+        let $slidePosition = $(".slide_img").index($currentSlide)
+            if ($slidePosition === $allSlides.length - 1) {
+                $($allSlides[$slidePosition]).removeClass("active_slide")
+                $slidePosition = 0;
+                $($allSlides[$slidePosition]).addClass("active_slide")
+            } else {
+                $($allSlides[$slidePosition]).removeClass("active_slide")
+                $slidePosition ++;
+                $($allSlides[$slidePosition]).addClass("active_slide")
+            }
+            console.log("Dernière valeur $slidePosition = " + $slidePosition);
+    }
+
+    slideLeftChange() {
+        let $allSlides = $(".slide_img") // A mettre dans conctrustor ??
+        let $currentSlide = $(".active_slide");
+        let $slidePosition = $(".slide_img").index($currentSlide)
+            if ($slidePosition === 0) {
+                $($allSlides[$slidePosition]).removeClass("active_slide")
+                $slidePosition = $allSlides.length - 1;
+                $($allSlides[$slidePosition]).addClass("active_slide")
+            } else {
+                $($allSlides[$slidePosition]).removeClass("active_slide")
+                $slidePosition --;
+                $($allSlides[$slidePosition]).addClass("active_slide")
+            }
+            console.log("Dernière valeur $slidePosition = " + $slidePosition);
+    }
+
+    sliderKeyboardChange() {
+        if (event.keyCode === 39) {
+            console.log(event.keyCode + " => Right Slider Change !");
+            this.slideRightChange();
+        } if (event.keyCode === 37) {
+            console.log(event.keyCode + " => Left Slider Change !");
+            this.slideLeftChange();
+        } if (event.keyCode === 32) {
+            console.log(event.keyCode + " => Play/Pause Slider !");
+            this.sliderAutoChange();
+        }     
+    }
+}
+       
+
