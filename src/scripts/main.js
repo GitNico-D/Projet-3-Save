22a3b8b49ab43a2a6d5a3953b7e3f96085609811
GitNico-D <niicolas.dubief@gmail.com>
2019-12-21 19:58:32 +0100

    Ajout d'un map de test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 $(function(){
     let slideOne = new Slide ("slide_one", "blue", 1);
-    // let slideTwo = new Slide ("slide_two", "red", 2);
-    // let slideThree = new Slide ("slide_three", "yellow", 3);
-    // let slideFour = new Slide ("slide_four", "green", 4);
-    // let slideFifth = new Slide ("slide_fifth", "green", 4);
 }); 
+
+
+$(function(){
+    let testingMap = new Map ([51.505, -0.09], 13);
+}); 
+// let testingMap = new Map ([51.505, -0.09], 13);
