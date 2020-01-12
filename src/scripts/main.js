6e10f74f4ca84cbdac4ddaa8df644153c3b7e130
GitNico-D <niicolas.dubief@gmail.com>
2020-01-12 19:15:03 +0100

    -

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
-let url = "https://api.jcdecaux.com/vls/v3/stations?contract=Lyon&apiKey=02097fba5ab73ba010c3d0375ab4c4aa19dd9f23"
+// let url = "https://api.jcdecaux.com/vls/v3/stations?contract=Lyon&apiKey=02097fba5ab73ba010c3d0375ab4c4aa19dd9f23"
 
 $(function(){
     let slideOne = new Slide ();
     let mapOfLyon = new Map ([45.75, 4.85], 13);
-    let datasStations = new Station(url, mapOfLyon);
+    let datasStations = new Station(mapOfLyon);
 });
 
