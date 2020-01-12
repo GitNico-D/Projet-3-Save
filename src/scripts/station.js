132ddc6855f6a1462d13fa4a48fd73c3817352f8
GitNico-D <niicolas.dubief@gmail.com>
2020-01-12 19:12:21 +0100

    Gestion des markers sur la carte en fonction de l'état des stations

--- src/scripts/station.js
+++ src/scripts/station.js
@@ -1,20 +1,21 @@
 class Station {
-    constructor(url, map) {
+    constructor(map) {
+        // console.log(self)
         const self = this;
-        console.log(self)
         this.map = map;
         // this.stationMarker = L.icon({iconUrl: `assets/img/leaf-${this.colorStationMarker}.png`});
-        $.get(url)
+        $.get("https://api.jcdecaux.com/vls/v3/stations?contract=Lyon&apiKey=02097fba5ab73ba010c3d0375ab4c4aa19dd9f23")
             .done (function(allStationsData){
                 for (let i = 0; i < allStationsData.length; i++) {
                     let stationData = allStationsData[i]; 
+                    // console.log(stationData)
                     let stationLatitude = stationData.position.latitude;
                     let stationLongitude = stationData.position.longitude;
                     let colorStationMarker = "";
                     switch (stationData.status) {
                         case "OPEN": 
-                            console.log("La station " + stationData.name + " est " + stationData.status)
-                            console.log(stationData.totalStands.availabilities.bikes + " vélos sur " + stationData.totalStands.capacity + " sont disponiles à la location.");
+                            // console.log("La station " + stationData.name + " est " + stationData.status)
+                            // console.log(stationData.totalStands.availabilities.bikes + " vélos sur " + stationData.totalStands.capacity + " sont disponiles à la location.");
                             if (stationData.totalStands.availabilities.bikes === 0) {
                                 colorStationMarker = L.icon({iconUrl: `assets/img/leaf-orange.png`});
                             } else {
@@ -23,19 +24,24 @@ class Station {
                         break;
                         case "CLOSED":
                             colorStationMarker = L.icon({iconUrl: `assets/img/leaf-red.png`});
-                            console.log("La station " + stationData.name + " est " + stationData.status)
-                            console.log(stationData.totalStands.availabilities.bikes + " vélos sur " + stationData.totalStands.capacity + " sont disponiles à la location.");
+                            // console.log("La station " + stationData.name + " est " + stationData.status)
+                            // console.log(stationData.totalStands.availabilities.bikes + " vélos sur " + stationData.totalStands.capacity + " sont disponiles à la location.");
                         break;
-                    }
-                    self.map.generateStationMarker(stationLatitude,stationLongitude,colorStationMarker);
-                    // self.map.on()
+                    }                    
+                    self.map.generateStationMarker(stationData,colorStationMarker);
                 }
             })
             .fail(function(){
                 console.log("Error ! ", error);
-            })
-            
-        
+            })        
     }
-        
+    
+    // showInfosStation(stationData) {
+    //     $(".text").text(stationData.name);
+    // }
+
+    // showInfosStation = (stationData) => {
+    //     $(".text").text(stationData.name);
+    // }
+            
 }
