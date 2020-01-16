7a3ebed9d29d6e00675a0bfbc594829e26f8f1d3
GitNico-D <niicolas.dubief@gmail.com>
2020-01-16 16:56:59 +0100

    Améliorations de l'event click marker, et de l'affichage des infos des stations en fonctions de leurs status

--- src/scripts/station.js
+++ src/scripts/station.js
@@ -1,89 +1,95 @@
-// import { marker } from "leaflet";
-
 class Station {
     constructor(map) {
         // console.log(self)
         const self = this;
         this.map = map;
-        // this.stationMarker = L.icon({iconUrl: `assets/img/leaf-${this.colorStationMarker}.png`});
         $.get("https://api.jcdecaux.com/vls/v3/stations?contract=Lyon&apiKey=02097fba5ab73ba010c3d0375ab4c4aa19dd9f23")
             .done (function(allStationsData){
                 for (let i = 0; i < allStationsData.length; i++) {
                     let stationData = allStationsData[i]; 
                     // console.log(stationData)
-                    let stationLatitude = stationData.position.latitude;
-                    let stationLongitude = stationData.position.longitude;
+                    let stationOptionInformationsShow; 
                     let colorStationMarker = "";
-                    // let allMarkersStations = [];
                     switch (stationData.status) {
                         case "OPEN": 
-                            // console.log("La station " + stationData.name + " est " + stationData.status)
-                            // console.log(stationData.totalStands.availabilities.bikes + " vélos sur " + stationData.totalStands.capacity + " sont disponiles à la location.");
                             if (stationData.totalStands.availabilities.bikes === 0) {
                                 colorStationMarker = L.icon({iconUrl: `assets/img/leaf-orange.png`});
+                                stationOptionInformationsShow = 1;
                             } else {
                                 colorStationMarker = L.icon({iconUrl: `assets/img/leaf-green.png`});
+                                stationOptionInformationsShow = 2;
                             }
                         break;
                         case "CLOSED":
                             colorStationMarker = L.icon({iconUrl: `assets/img/leaf-red.png`});
-                            // console.log("La station " + stationData.name + " est " + stationData.status)
-                            // console.log(stationData.totalStands.availabilities.bikes + " vélos sur " + stationData.totalStands.capacity + " sont disponiles à la location.");
+                            stationOptionInformationsShow = 0;
                         break;
                     }                    
                     // self.map.generateStationMarker(stationData,colorStationMarker);
+                    $("#return_to_map").click(function(){
+                        self.map.resetMapView();
+                    });
                     let markerStation = self.map.generateStationMarker(stationData,colorStationMarker);
-                    // allMarkersStations.push(markerStation);
-                    // console.log(markerStation)
-                    // console.log(self.map)
-                    // console.log(self.map.bikeStationMap)
-                    // let bikeStationMap = self.map.bikeStationMap
-                    // console.log(allMarkersStations)
-                    // let stationName = $(".text").text(stationData.name)
-                    // var popup = L.popup()
-                        // .setContent()
-                        // .openOn(self.map)
-                    // markerStation.bindPopup(popup).openPopup()
-                    // self.eventClickMarker(markerStation, bikeStationMap)
                     $("#bike_station_details").hide();
-                    $("#user_form").hide();
+                    $("#user_form_booking").hide();
                     markerStation.on("click", function(event){
                         self.map.bikeStationMap.setView(event.latlng, 20)
-                        self.showInfosStation(stationData, markerStation)
-                        // L.popup("Coucou").openPopup(self.map)
-                        // markerStation.bindPopup("Coucou").openOn(self.map)
+                        self.showInfosStation(stationData, markerStation, stationOptionInformationsShow, self.map)
                     })
-                    // self.map.bikeStationMap.on("click", marker, self.showInfosStation(stationData))
-                    // self.map.bikeStationMap.on("click", marker, function(){
-                    //     console.log("Marker !")
-                    // })
-
                 }
+                    // self.map.bikeStationMap.on('click', function(event){
+                    //     console.log("Map click " + event.latlng)
+                    // })
             })
             .fail(function(){
                 console.log("Error ! ", error);
             })        
     }
     
-    showInfosStation(stationData, markerStation) {
+    showInfosStation(stationData, markerStation, stationOptionInformationsShow, bikeStationMap) {
         let stationPopupInfos = L.popup()
-            .setContent(`<p class=stationPopupInfos ><b> Vous êtes sur la station : </b></p>
-                        <p>${stationData.name}</p>
-                        <button id=station_details_informations class=station_details_informations> Informations détaillées >`);
-                                
+            .setContent(`<p class=popup_infos_station ><b> Vous êtes sur la station : </b></p>
+                        <p class=popup_infos_station_name>${stationData.name}</p>
+                        <button id=station_details_informations class=station_details_informations> Informations détaillées
+                        <button id=return_to_global_map class=return_to_global_map> Recentrer la carte`)
         markerStation.bindPopup(stationPopupInfos).openPopup()
+        
+        $("#return_to_global_map").click(function(){
+            bikeStationMap.resetMapView();
+            $("#bike_station_details").hide()
+            // $("#bike_station_informations").empty()
+        });
         $("#station_details_informations").click(function(){
-            $("#bike_station_details").show();
-            $(".text").text(`La station ${stationData.name} est ${stationData.status} !`)
-            $(".text").append(`<p> Il reste ${stationData.totalStands.availabilities.bikes} vélos disponibles à la location</p>`);
-            $(".text").append(`<p> Si vous souhaitez effectué une réservation cliquez sur le bouton "Réserver" ci-dessous.</p>`);
-            $(".text").append(`<button id=reservation_button class> Réserver`);
-            
-        })
+            if (stationOptionInformationsShow === 0) {
+                $("#bike_station_details").show();
+                    $("#bike_station_informations").append("<p>Désolé mais cette station est actuellement fermé. Veuillez nous excuser pour le dérangement.</p>");
+                    $("#bike_station_informations").append("<p> Merci de vous rapprocher d'une autre station de location.");
+                    $("#bike_station_informations").append("<button id=return_map class=return_to_map_station> Retour sur la carte")
+            } if (stationOptionInformationsShow === 1) {
+                $("#bike_station_details").show();
+                $("#bike_station_informations").append(`La station ${stationData.name} est bien ouverte !`)
+                $("#bike_station_informations").append(`<p> Il reste malheureusement ${stationData.totalStands.availabilities.bikes} disponibles à la location. Merci de vous rapprocher d'une autre station de location.</p>`);
+                $("#bike_station_informations").append(`<p> Nous vous souhaitons une bonne journée !</p>`);
+                $("#bike_station_informations").append(`<button id=reservation_button > Réserver`);            
+                $("#bike_station_informations").append("<button id=return_map class=return_to_map_station> Reotur sur la carte")
+            } if (stationOptionInformationsShow === 2) {
+                $("#bike_station_details").show();
+                $("#bike_station_informations").append(`La station ${stationData.name} est Ouverte !`)
+                $("#bike_station_informations").append(`<p> Il reste ${stationData.totalStands.availabilities.bikes} vélos disponibles à la location.</p>`);
+                $("#bike_station_informations").append(`<p> Si vous souhaitez effectuer une réservation cliquez sur le bouton "Réserver" ci-dessous.</p>`);
+                $("#bike_station_informations").append(`<button id=reservation_button > Réserver`);            
                 $("#reservation_button").click(function(){
-                    $("#user_form").show();
+                    $("#user_form_booking").show();
                     console.log("Clique réservation")
-
+                })
+            }
         });        
+    }
+
+    clearInfosStation(bikeStationMap){
+        bikeStationMap.on('click', function(event){
+            console.log("Map Click" + event.latlng)
+            // $("#bike_station_details").hide()
+        })
     }
 }
