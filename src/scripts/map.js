72377a8687801739d402a316ad224ac15e747a35
GitNico-D <niicolas.dubief@gmail.com>
2020-01-17 18:54:54 +0100

    Ajout de plusieurs methodes, eventClickMarker, resetMapView, closedInfosStation et event sur des button pour reset la MapView

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -5,6 +5,10 @@ class Map {
         this.zoom = zoom;
         this.bikeStationMap = L.map("bike_station_map").setView([this.coordinateX, this.coordinateY], this.zoom);
         this.generateMap();
+        console.log(this)
+        $("#return_to_map").click(this.resetMapView.bind(this));
+        $("#return_to_global_map").click(this.resetMapView.bind(this))
+        this.bikeStationMap.on("click", this.closedInfosStation.bind(this))
     }
 
 
@@ -16,25 +20,50 @@ class Map {
             id: 'mapbox/streets-v11',
             accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
         }).addTo(this.bikeStationMap);
+        console.log(this.bikeStationMap)
     }
     
     generateStationMarker(stationData, stationMarker) {
-        // let marker = 
-        // console.log(stationData)
         return L.marker([stationData.position.latitude,stationData.position.longitude], {icon: stationMarker}).addTo(this.bikeStationMap);
-        // marker.on("click", function(event){
-        //     console.log(stationData.address)
-        //     $(".text").html(`${stationData.name}<br> Cette station a ${stationData.totalStands.availabilities.bikes} vélos disponibles sur  ${stationData.totalStands.capacity}`);
-        //     $(".text").append().text(stationData.address);
-        // } );
-        // this.bikeStationMap.on('click', function(event){
-        //     console.log(event.latlng)
-        // })
-        // markerEventClick(marker)
-        // console.log(marker);
     }
 
+    eventMarkerClick(markerStation, event, stationName, showInfosStation) {
+        let map = this.bikeStationMap
+        // console.log(map)
+        // console.log(this.bikeStationMap)
+        markerStation.on(event, function(event) {
+            // showInfosStation()
+            map.setView(event.latlng, 20)
+            // console.log(markerStation)
+            let stationPopupInfos = L.popup()
+                .setContent(`<p class=popup_infos_station ><b> Vous êtes sur la station : </b></p>
+                            <p class=popup_infos_station_name>${stationName}</p>
+                            <button id=station_details_informations class=station_details_informations> Informations détaillées`)                            
+            markerStation.bindPopup(stationPopupInfos).openPopup()
+            $("#station_details_informations").click(function(){
+                console.log("Click info détaillé")
+                showInfosStation()
+            });
+        });
+        // console.log(markerStation)
+        // this.markerPopup();
+    }
+
+    closedInfosStation() {
+        $("#bike_station_details").hide();
+    }
+
+    // markerPopup(stationData, markerStation){
+    //     let stationPopupInfos = L.popup()
+    //         .setContent(`<p class=popup_infos_station ><b> Vous êtes sur la station : </b></p>
+    //                     <p class=popup_infos_station_name>${stationData.name}</p>
+    //                     <button id=station_details_informations class=station_details_informations> Informations détaillées
+    //                     <button id=return_to_global_map class=return_to_global_map> Recentrer la carte`)
+    //     markerStation.bindPopup(stationPopupInfos).openPopup()
+    // }
+
     resetMapView(){
+        console.log("Click Return to global map button")
         this.bikeStationMap.setView([this.coordinateX, this.coordinateY], this.zoom);
     }
 }
