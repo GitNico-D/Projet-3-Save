22d07653cc767f56a1618e43322333b3102badce
GitNico-D <niicolas.dubief@gmail.com>
2020-01-23 16:51:06 +0100

    Ajout de la méthode de booking et de vérification des infos tapé dans le formulaire

--- src/scripts/booking.js
+++ src/scripts/booking.js
@@ -1,15 +1,38 @@
 class Booking {
     constructor() {
-        // this.firstName = firstName;
-        // this.lastName = lastName;
-        this.displayBooking();
+        this.displayBookingForm();
+        $("#first_name").keyup(this.userData.bind(this));
+        $("#last_name").keyup(this.userData.bind(this));
     }
 
-    displayBooking() {
+    displayBookingForm() {
         $("#reservation_button").click(function() {
-            $("#user_form_booking").show();
-
+            $("#bike_station_details").hide();
+            $("#user_booking_form").show();
             console.log("Booking")
+        })
+    }
+
+    userData() {
+        console.log($("#first_name").val());
+        console.log($("#last_name").val());
+    }
+
+    userBookingStation(stationData) {
+        $("#reservation_confirm").hide();
+        // $("#revervation_cancel").hide();
+        $("form").submit(function(event) { 
+            if ($("#first_name").val() != "" && $("#last_name").val() != "") {
+                console.log($("#first.name").val());
+                console.log($("#last_name").val());
+                console.log("Affichage Canvas")
+                $("#reservation_status_text").text(`Vous souhaitez effectuer la réservation d'un vélo à la ${stationData.name} ?`);
+                $("#reservation_confirm").show();
+                $("#reservation_cancel").show();
+            } else {
+                $("#reservation_status_text").text("Les champs Prénom et Nom sont obligatoires ! ");
+            }
+            event.preventDefault()
         })
     }
 }
