d519ce8c1fe583441f6e405d8116391838fbf10d
GitNico-D <niicolas.dubief@gmail.com>
2020-01-30 16:49:12 +0100

    Méthode de dessin du canvas fonctionnel

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -9,6 +9,7 @@ class Canvas {
         $("#signature_canvas").mousedown(this.mouseDown.bind(this));
         $("#signature_canvas").mousemove(this.mouseMove.bind(this));
         $("#signature_canvas").mouseup(this.mouseUp.bind(this));
+        $("#signature_canvas").mouseleave(this.mouseLeave.bind(this));
         $("#clear_canvas").click(this.clearCanvas.bind(this));
         // this.testingDraw()
     }
@@ -19,6 +20,20 @@ class Canvas {
     //     this.context.lineTo(250,75);
     //     this.context.stroke()
     // }
+
+    // showCanvas() {
+    //     console.log("Affichage Canvas " )
+    //     $("#signature_canvas").css("display", "block")
+    // }
+
+    signatureValidation() {
+        console.log("Show Confirmation or Cancelation");
+        $("#confirm_canvas").show().fadeIn(1000, "linear");
+        $("#clear_canvas").show().fadeIn("slow");
+        $("#clear_canvas").click(function(){
+            $("#confirm_canvas").hide().fadeOut(1000, "linear");
+        })
+    }
 
     mouseDown() {
         this.startSignature = true;
@@ -39,8 +54,15 @@ class Canvas {
 
     mouseUp() {
         this.startSignature = false;
+        this.signatureValidation();
         console.log("Stoping signature !");
+        this.context.removeHitRegion()
         $("#canvas_text_3").text("MouseUp position => X : " + event.clientX + " , " + " Y : " + event.clientY);
+    }
+
+    mouseLeave() {
+        this.startSignature = false;
+        this.signatureValidation();
     }
 
     clearCanvas() {
