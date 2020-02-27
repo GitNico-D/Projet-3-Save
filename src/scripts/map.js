54e1f3d9c3138b3a4ae3181697537334e742038e
GitNico-D <niicolas.dubief@gmail.com>
2020-02-27 17:01:02 +0100

    Suppression comments

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -5,29 +5,25 @@ class Map {
         this.zoom = zoom;
         this.bikeStationMap = L.map("bike_station_map").setView([this.coordinateX, this.coordinateY], this.zoom);
         this.generateMap();
-        console.log(this)
         $("#return_to_map_button").click(this.resetMapView.bind(this));
-        $("#alert_return_map_button").click(this.resetMapView.bind(this))
+        $("#alert_return_map_button").click(this.resetMapView.bind(this));
         this.bikeStationMap.on("click", this.closedInfosStation.bind(this));
     }
     
 
     generateMap() {
-    console.log(this)
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox/streets-v11',
             accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
         }).addTo(this.bikeStationMap);
-        console.log(this.bikeStationMap)
     }
     
     generateStationMarker(stationData, stationMarker) {
         let popup = L.popup().setContent(`<p class=popup_infos_station> Station sélectionné : </p>
                                             <p class=popup_infos_station_name>${stationData.name}</p>`);
         return L.marker([stationData.position.latitude,stationData.position.longitude], {icon: stationMarker}).bindPopup(popup);
-        // return L.marker([stationData.position.latitude,stationData.position.longitude], {icon: stationMarker}).addTo(this.bikeStationMap).bindPopup(popup);
     }
 
     eventMarkerClick(markerStation, event, showInfosStation) {
@@ -39,7 +35,6 @@ class Map {
     }
 
     closedInfosStation() {
-        console.log("Click Map")
         $("#bike_station_map").removeClass("col-lg-9").addClass("col");
         $("#bike_station_details").hide();
     }
@@ -53,7 +48,6 @@ class Map {
     }
 
     resetMapView(){
-        console.log("Click Return to global map button")
         this.bikeStationMap.setView([this.coordinateX, this.coordinateY], this.zoom);
         $("#bike_station_details").hide();
         $("#bike_station_map").addClass("col").removeClass("col-lg-9");
