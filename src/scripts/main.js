a30024ad8e25572bb6c4f91a2898eb7bd56af1bb
GitNico-D <niicolas.dubief@gmail.com>
2020-03-03 19:38:52 +0100

    Reupload

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,3 @@
-// let url = "https://api.jcdecaux.com/vls/v3/stations?contract=Lyon&apiKey=02097fba5ab73ba010c3d0375ab4c4aa19dd9f23"
-
 $(function(){
     let slideOne = new Slide ();
     let mapOfLyon = new Map ([45.77, 4.85], 11);
@@ -7,4 +5,3 @@ $(function(){
     let bookingBike = new Booking();
     let datasStations = new Station(mapOfLyon, bookingBike);
 });
-
