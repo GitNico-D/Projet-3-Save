2348574121fed2abb0bca102671d3f6382e30f6d
GitNico-D <niicolas.dubief@gmail.com>
2020-03-05 17:00:19 +0100

    Ajout des méthodes touchEvent, non fonctionnel pour l'instant

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -9,6 +9,9 @@ class Canvas {
         $("#signature_canvas").mousedown(this.mouseDown.bind(this));
         $("#signature_canvas").mousemove(this.mouseMove.bind(this));
         $("#signature_canvas").mouseup(this.mouseUp.bind(this));
+        $("#signature_canvas").on("touchstart", this.touchStart.bind(this));
+        $("#signature_canvas").on("touchmove", this.touchMove.bind(this));
+        $("#signature_canvas").on("touchEnd", this.touchEnd.bind(this));
         $("#clear_canvas").click(this.clearCanvas.bind(this));
     }
     
@@ -24,10 +27,18 @@ class Canvas {
 
     mouseDown() {
         this.startSignature = true;
-        this.context.moveTo(event.offsetX, event.offsetY)
-        this.context.beginPath()
+        this.context.moveTo(event.offsetX, event.offsetY);
+        this.context.beginPath();
         // console.log("Starting signature !")
+        // $("#canvas_text").text("MouseDown position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
+    }
+
+    touchStart() {
+        this.startSignature = true;
+        this.context.moveTo(event.touches[0].offsetX, event.touches[0].offsetY);
+        this.context.beginPath();
         $("#canvas_text").text("MouseDown position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
+
     }
 
     mouseMove() {
@@ -36,14 +47,30 @@ class Canvas {
             this.context.stroke();
             // console.log("During signature !")
         }
-        $("#canvas_text_2").text("MouseMove position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
+        // $("#canvas_text_2").text("MouseMove position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
+    }
+
+    touchMove() {
+        if (this.startSignature === true) {
+            this.context.fill(e.touches[0].offsetX, e.touches[0].offsetY);
+            // this.context.lineTo(e.touches[0].offsetX, e.touches[0].offsetY);
+            this.context.stroke();
+            // console.log("During signature !")
+        }
     }
 
     mouseUp() {
         this.startSignature = false;
         // console.log("Stoping signature !");
-        $("#canvas_text_3").text("MouseUp position => X : " + event.clientX + " , " + " Y : " + event.clientY);
+        // $("#canvas_text_3").text("MouseUp position => X : " + event.clientX + " , " + " Y : " + event.clientY);
         this.signatureValidation();
+    }
+
+    touchEnd() {
+        this.startSignature = false;
+        this.context.close
+        this.signatureValidation();
+
     }
 
     clearCanvas() {
