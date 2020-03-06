ea89a5082f68c910cc7ffe661d2d6e6ade5593c7
GitNico-D <niicolas.dubief@gmail.com>
2020-03-06 21:33:21 +0100

    Correction du keyboardChange

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,63 +1,65 @@
 class Slide {
-    constructor (slideId, color, position) {
-        this.slideId = slideId;
-        this.color = color;
-        this.position = position;
-        $("#next_slide").click(this.sliderChange.bind(this, 1));
-        $("#previous_slide").click(this.sliderChange.bind(this, -1));
-        this.start = false;
-        this.sliderAutoChange();
-        $("#play_pause_slider").click(this.sliderAutoChange.bind(this));
-        $(document).keydown(this.sliderKeyboardChange.bind(this));
-    }
+  constructor(slideId, color, position) {
+    this.slideId = slideId;
+    this.color = color;
+    this.position = position;
+    $("#next_slide").click(this.sliderChange.bind(this, 1));
+    $("#previous_slide").click(this.sliderChange.bind(this, -1));
+    this.start = false;
+    this.sliderAutoChange();
+    $("#play_pause_slider").click(this.sliderAutoChange.bind(this));
+    $(document).keydown(this.sliderKeyboardChange.bind(this));
+  }
 
-    sliderAutoChange() {
-        if (!this.start) {
-            $("#play_pause_slider").removeClass("fa-play-circle");         
-            this.interval = setInterval(this.sliderChange.bind(this, 1), 5000)
-            $("#play_pause_slider").addClass("fa-pause-circle");
-            console.log("Start Slider !");
-        } else {
-            $("#play_pause_slider").removeClass("fa-pause-circle");
-            clearInterval(this.interval); 
-            $("#play_pause_slider").addClass("fa-play-circle");
-            console.log("Pause Slider !")             
-        }
-        this.start=!this.start;
+  sliderAutoChange() {
+    if (!this.start) {
+      $("#play_pause_slider").removeClass("fa-play-circle");
+      this.interval = setInterval(this.sliderChange.bind(this, 1), 5000);
+      $("#play_pause_slider").addClass("fa-pause-circle");
+      console.log("Start Slider !");
+    } else {
+      $("#play_pause_slider").removeClass("fa-pause-circle");
+      clearInterval(this.interval);
+      $("#play_pause_slider").addClass("fa-play-circle");
+      console.log("Pause Slider !");
     }
-    
-    sliderChange(direction) {
-        let $allSlides = $(".slide_img"); 
-        let $currentSlide = $(".active_slide");
-        let $slidePosition = $(".slide_img").index($currentSlide);
-        $($allSlides[$slidePosition]).removeClass("active_slide")
-        $slidePosition += direction;
-        $($allSlides[$slidePosition]).addClass("active_slide") 
-        if ($slidePosition >= $allSlides.length) {
-            $($allSlides[$slidePosition]).removeClass("active_slide")  
-            $slidePosition = 0;
-            $($allSlides[$slidePosition]).addClass("active_slide")
-        } else if ($slidePosition < 0) {
-            $($allSlides[$slidePosition]).removeClass("active_slide")
-            $slidePosition = $allSlides.length - 1;
-            $($allSlides[$slidePosition]).addClass("active_slide")
-        }                
-    }
+    this.start = !this.start;
+  }
 
-    sliderKeyboardChange() {
-        // event.stopPropagation();
-        if (event.keyCode === 39) {
-            console.log(event.keyCode + " => Right Slider Change !");
-            this.sliderChange(1);
-        } if (event.keyCode === 37) {
-            console.log(event.keyCode + " => Left Slider Change !");
-            this.sliderChange(-1);
-        } if (event.keyCode === 32) {
-            event.preventDefault();
-            event.stopPropagation();
-            console.log(event.keyCode + " => Play/Pause Slider !");
-            this.sliderChange();
-
-        }     
+  sliderChange(direction) {
+    let $allSlides = $(".slide_img");
+    let $currentSlide = $(".active_slide");
+    let $slidePosition = $(".slide_img").index($currentSlide);
+    $($allSlides[$slidePosition]).removeClass("active_slide");
+    $slidePosition += direction;
+    $($allSlides[$slidePosition]).addClass("active_slide");
+    if ($slidePosition >= $allSlides.length) {
+      $($allSlides[$slidePosition]).removeClass("active_slide");
+      $slidePosition = 0;
+      $($allSlides[$slidePosition]).addClass("active_slide");
+    } else if ($slidePosition < 0) {
+      $($allSlides[$slidePosition]).removeClass("active_slide");
+      $slidePosition = $allSlides.length - 1;
+      $($allSlides[$slidePosition]).addClass("active_slide");
     }
+  }
+
+  sliderKeyboardChange() {
+    // event.stopPropagation();
+    if (event.keyCode === 39) {
+      event.preventDefault();
+      console.log(event.keyCode + " => Right Slider Change !");
+      this.sliderChange(1);
+    }
+    if (event.keyCode === 37) {
+      event.preventDefault();
+      console.log(event.keyCode + " => Left Slider Change !");
+      this.sliderChange(-1);
+    }
+    if (event.keyCode === 32) {
+      event.preventDefault();
+      console.log(event.keyCode + " => Play/Pause Slider !");
+      this.sliderAutoChange();
+    }
+  }
 }
