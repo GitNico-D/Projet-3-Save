ca20e1c47dec6e2b0be490b8d2cd17f3275353c6
GitNico-D <niicolas.dubief@gmail.com>
2020-03-06 20:55:10 +0100

    Travail sur les méthodes touchEvent

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -6,6 +6,7 @@ class Canvas {
         this.context.lineJoin = "round";
         this.context.strokeStyle = "black";
         this.startSignature = false;
+        this.rect = this.canvasId.getBoundingClientRect();
         $("#signature_canvas").mousedown(this.mouseDown.bind(this));
         $("#signature_canvas").mousemove(this.mouseMove.bind(this));
         $("#signature_canvas").mouseup(this.mouseUp.bind(this));
@@ -17,11 +18,11 @@ class Canvas {
     
     signatureValidation() {
         // console.log("Show Confirmation or Cancelation");
-        $("#confirm_canvas").show().fadeIn(1000, "linear");
-        $("#clear_canvas").show().fadeIn("slow");
+        $("#confirm_canvas").toggleClass("hide", false);
+        $("#clear_canvas").toggleClass("hide", false);
         $("#clear_canvas").click(function(){
-            $("#confirm_canvas").hide().fadeOut(1000, "linear");
-            $("#clear_canvas").hide().fadeOut(1000, "linear");
+            $("#confirm_canvas").toggleClass("hide", true);
+            $("#clear_canvas").toggleClass("hide", true);
         })
     }
 
@@ -30,14 +31,18 @@ class Canvas {
         this.context.moveTo(event.offsetX, event.offsetY);
         this.context.beginPath();
         // console.log("Starting signature !")
-        // $("#canvas_text").text("MouseDown position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
+        console.log("MouseDown position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
     }
 
     touchStart() {
+        // let rect = canvasId.getBoundingoffsetRect();
         this.startSignature = true;
-        this.context.moveTo(event.touches[0].offsetX, event.touches[0].offsetY);
+        this.context.moveTo(event.touches[0].clientX, event.touches[0].clientY);
+        this.context.moveTo((event.touches[0].clientX - this.canvasId.width), (event.touches[0].clientY - this.canvasId.height));
         this.context.beginPath();
-        $("#canvas_text").text("MouseDown position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
+        console.log(outer.width, this.canvasId.height)
+        console.log("TouchStart position => X : " + event.touches[0].offsetX  + " , " + " Y : " + event.touches[0].offsetY);
+        console.log("TouchStart position => X : " + (event.touches[0].clientX - this.canvasId.height)  + " , " + " Y : " + (event.touches[0].clientY - this.canvasId.height));
 
     }
 
@@ -47,15 +52,15 @@ class Canvas {
             this.context.stroke();
             // console.log("During signature !")
         }
-        // $("#canvas_text_2").text("MouseMove position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
+        // $("#canvas_text_2").text("MouseMove position => X : " + event.clientX + " , " + " Y : " + event.clientY);
     }
 
     touchMove() {
         if (this.startSignature === true) {
-            this.context.fill(e.touches[0].offsetX, e.touches[0].offsetY);
-            // this.context.lineTo(e.touches[0].offsetX, e.touches[0].offsetY);
+            // this.context.fill(event.touches[0].clientX, event.touches[0].clientY);
+            this.context.lineTo(event.touches[0].clientX, event.touches[0].clientY);
             this.context.stroke();
-            // console.log("During signature !")
+            console.log("Signature en cours !")
         }
     }
 
@@ -68,13 +73,15 @@ class Canvas {
 
     touchEnd() {
         this.startSignature = false;
-        this.context.close
+        this.context.closedPath();
         this.signatureValidation();
 
     }
 
     clearCanvas() {
         console.log("Clear Clear")
-        this.context.clearRect(0, 0, this.canvasId.width, this.canvasId.height)
+        this.context.clearRect(0, 0, this.canvasId.width, this.canvasId.height);
+        $("#confirm_canvas").toggleClass("hide", true);
+        $("#clear_canvas").toggleClass("hide", true);
     }
 }
