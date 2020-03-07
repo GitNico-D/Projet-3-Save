1606469e52d992c31eca527970627c6ab517e743
GitNico-D <niicolas.dubief@gmail.com>
2020-03-07 21:36:32 +0100

    nc

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  let slideOne = new Slide();
+  let slider = new Slider();
   let mapOfLyon = new Map([45.77, 4.85], 11);
   let signatureCanvas = new Canvas(signature_canvas);
   let bookingBike = new Booking(mapOfLyon, signatureCanvas);
