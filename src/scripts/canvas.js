763910ff0374915c12ef2669516dd0b8d38d5cdc
GitNico-D <niicolas.dubief@gmail.com>
2020-03-07 21:36:13 +0100

    Ajout méthode touchEvent fonctionnel et gestion d'un certain nbrs de points pour la signature

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -6,23 +6,25 @@ class Canvas {
         this.context.lineJoin = "round";
         this.context.strokeStyle = "black";
         this.startSignature = false;
-        this.rect = this.canvasId.getBoundingClientRect();
+        this.signaturePoints = 0;
         $("#signature_canvas").mousedown(this.mouseDown.bind(this));
         $("#signature_canvas").mousemove(this.mouseMove.bind(this));
         $("#signature_canvas").mouseup(this.mouseUp.bind(this));
         $("#signature_canvas").on("touchstart", this.touchStart.bind(this));
         $("#signature_canvas").on("touchmove", this.touchMove.bind(this));
-        $("#signature_canvas").on("touchEnd", this.touchEnd.bind(this));
+        $("#signature_canvas").on("touchend", this.touchEnd.bind(this));
         $("#clear_canvas").click(this.clearCanvas.bind(this));
     }
     
     signatureValidation() {
-        // console.log("Show Confirmation or Cancelation");
+        console.log("Show Confirmation or Cancelation");
         $("#confirm_canvas").toggleClass("hide", false);
         $("#clear_canvas").toggleClass("hide", false);
         $("#clear_canvas").click(function(){
             $("#confirm_canvas").toggleClass("hide", true);
             $("#clear_canvas").toggleClass("hide", true);
+            $("#alert").removeClass("alert-danger").removeClass("alert-warning").addClass("alert-info").html("Pour <span>valider</span> votre réservation, merci d'aposer votre <span>signature</span> dans le cadre ci-dessous :");
+            this.signaturePoints = 0;
         })
     }
 
@@ -30,58 +32,60 @@ class Canvas {
         this.startSignature = true;
         this.context.moveTo(event.offsetX, event.offsetY);
         this.context.beginPath();
-        // console.log("Starting signature !")
-        console.log("MouseDown position => X : " + event.offsetX + " , " + " Y : " + event.offsetY);
-    }
-
-    touchStart() {
-        // let rect = canvasId.getBoundingoffsetRect();
-        this.startSignature = true;
-        this.context.moveTo(event.touches[0].clientX, event.touches[0].clientY);
-        this.context.moveTo((event.touches[0].clientX - this.canvasId.width), (event.touches[0].clientY - this.canvasId.height));
-        this.context.beginPath();
-        console.log(outer.width, this.canvasId.height)
-        console.log("TouchStart position => X : " + event.touches[0].offsetX  + " , " + " Y : " + event.touches[0].offsetY);
-        console.log("TouchStart position => X : " + (event.touches[0].clientX - this.canvasId.height)  + " , " + " Y : " + (event.touches[0].clientY - this.canvasId.height));
-
-    }
+    }    
 
     mouseMove() {
         if (this.startSignature === true) {
             this.context.lineTo(event.offsetX, event.offsetY);
             this.context.stroke();
-            // console.log("During signature !")
+            this.signaturePoints ++;
         }
-        // $("#canvas_text_2").text("MouseMove position => X : " + event.clientX + " , " + " Y : " + event.clientY);
-    }
-
-    touchMove() {
-        if (this.startSignature === true) {
-            // this.context.fill(event.touches[0].clientX, event.touches[0].clientY);
-            this.context.lineTo(event.touches[0].clientX, event.touches[0].clientY);
-            this.context.stroke();
-            console.log("Signature en cours !")
-        }
-    }
+    }   
 
     mouseUp() {
         this.startSignature = false;
-        // console.log("Stoping signature !");
-        // $("#canvas_text_3").text("MouseUp position => X : " + event.clientX + " , " + " Y : " + event.clientY);
-        this.signatureValidation();
+        if (this.signaturePoints > 150) {
+            this.signatureValidation();
+        } else {
+            $("#alert").removeClass("alert-info").addClass("alert-warning").html("Désolé mais votre <span>signature</span> est trop <span>courte</span>, merci de recommencer.");
+            this.clearCanvas();
+        }  
+    }
+    
+    touchStart() {
+        event.preventDefault();
+        let rect = this.canvasId.getBoundingClientRect();
+        this.startSignature = true;
+        this.context.moveTo((event.touches[0].clientX - rect.left), (event.touches[0].clientX - rect.top));
+        this.signaturePoints ++;
+        this.context.beginPath();
+    }
+    
+    touchMove() {
+        event.preventDefault();
+        let rect = this.canvasId.getBoundingClientRect();
+        if (this.startSignature === true) {
+            this.context.lineTo((event.touches[0].clientX - rect.left), (event.touches[0].clientY - rect.top));
+            this.context.stroke();
+            this.signaturePoints ++;
+        }
     }
 
     touchEnd() {
+        event.preventDefault();
         this.startSignature = false;
-        this.context.closedPath();
-        this.signatureValidation();
-
+        if (this.signaturePoints > 150) {
+            this.signatureValidation();
+        } else {
+            $("#alert").removeClass("alert-info").addClass("alert-warning").html("Désolé mais votre <span>signature</span> est trop <span>courte</span>, merci de recommencer.");
+            this.clearCanvas();
+        }       
     }
 
     clearCanvas() {
-        console.log("Clear Clear")
         this.context.clearRect(0, 0, this.canvasId.width, this.canvasId.height);
         $("#confirm_canvas").toggleClass("hide", true);
         $("#clear_canvas").toggleClass("hide", true);
+        this.signaturePoints = 0;
     }
 }
