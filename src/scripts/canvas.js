4cb32180aa1a8469cbd493cd00279bef032f9fe6
GitNico-D <niicolas.dubief@gmail.com>
2020-03-11 20:04:27 +0100

    Suppression clearCanvas() en cas de signature trop courte

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -2,7 +2,7 @@ class Canvas {
     constructor(canvasId) {
         this.canvasId = canvasId;
         this.context = this.canvasId.getContext("2d");
-        this.context.lineWidth = "2";
+        this.context.lineWidth = "1";
         this.context.lineJoin = "round";
         this.context.strokeStyle = "black";
         this.startSignature = false;
@@ -15,71 +15,71 @@ class Canvas {
         $("#signature_canvas").on("touchend", this.touchEnd.bind(this));
         $("#clear_canvas").click(this.clearCanvas.bind(this));
     }
-    
+
     signatureValidation() {
         console.log("Show Confirmation or Cancelation");
         $("#confirm_canvas").toggleClass("hide", false);
         $("#clear_canvas").toggleClass("hide", false);
-        $("#clear_canvas").click(function(){
+        $("#clear_canvas").click(function () {
             $("#confirm_canvas").toggleClass("hide", true);
             $("#clear_canvas").toggleClass("hide", true);
             $("#alert").removeClass("alert-danger").removeClass("alert-warning").addClass("alert-info").html("Pour <span>valider</span> votre réservation, merci d'aposer votre <span>signature</span> dans le cadre ci-dessous :");
             this.signaturePoints = 0;
-        })
+        });
     }
 
     mouseDown() {
         this.startSignature = true;
         this.context.moveTo(event.offsetX, event.offsetY);
         this.context.beginPath();
-    }    
+    }
 
     mouseMove() {
         if (this.startSignature === true) {
             this.context.lineTo(event.offsetX, event.offsetY);
             this.context.stroke();
-            this.signaturePoints ++;
+            this.signaturePoints++;
         }
-    }   
+    }
 
     mouseUp() {
         this.startSignature = false;
-        if (this.signaturePoints > 150) {
+        if (this.signaturePoints > 100) {
             this.signatureValidation();
         } else {
             $("#alert").removeClass("alert-info").addClass("alert-warning").html("Désolé mais votre <span>signature</span> est trop <span>courte</span>, merci de recommencer.");
-            this.clearCanvas();
-        }  
+            // this.clearCanvas();
+        }
     }
-    
+
     touchStart() {
         event.preventDefault();
         let rect = this.canvasId.getBoundingClientRect();
         this.startSignature = true;
         this.context.moveTo((event.touches[0].clientX - rect.left), (event.touches[0].clientX - rect.top));
-        this.signaturePoints ++;
+        this.signaturePoints++;
         this.context.beginPath();
     }
-    
+
     touchMove() {
         event.preventDefault();
         let rect = this.canvasId.getBoundingClientRect();
         if (this.startSignature === true) {
             this.context.lineTo((event.touches[0].clientX - rect.left), (event.touches[0].clientY - rect.top));
             this.context.stroke();
-            this.signaturePoints ++;
+            this.signaturePoints++;
         }
     }
 
     touchEnd() {
         event.preventDefault();
         this.startSignature = false;
-        if (this.signaturePoints > 150) {
+        if (this.signaturePoints > 100) {
             this.signatureValidation();
         } else {
             $("#alert").removeClass("alert-info").addClass("alert-warning").html("Désolé mais votre <span>signature</span> est trop <span>courte</span>, merci de recommencer.");
-            this.clearCanvas();
-        }       
+            // this.clearCanvas();
+        }
     }
 
     clearCanvas() {
