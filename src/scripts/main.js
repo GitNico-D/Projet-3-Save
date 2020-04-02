f369b85515f72f2e6b993722c070646f6225fcd7
GitNico-D <niicolas.dubief@gmail.com>
2020-04-02 16:41:03 +0200

    Version Finale

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,6 @@ $(function() {
   let slider = new Slider();
   let mapOfLyon = new Map([45.77, 4.85], 11);
   let signatureCanvas = new Canvas(signature_canvas);
-  let bookingBike = new Booking(mapOfLyon, signatureCanvas);
+  let bookingBike = new Booking();
   let datasStations = new Station(mapOfLyon, bookingBike);
 });
